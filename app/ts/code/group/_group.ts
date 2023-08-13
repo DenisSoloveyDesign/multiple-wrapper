e5e402155fd3ea0046780aae4e14c7ad0af89993
DenSolovey <denis.solovey.design@gmail.com>
2023-08-13 20:45:49 +0300

    Fixed group ordering

--- app/ts/code/group/_group.ts
+++ app/ts/code/group/_group.ts
@@ -1,21 +1,26 @@
+import { sort } from 'forto-sorter';
 import { getNodeIndex } from '@figma-plugin/helpers';
 import NodeBundle from '../nodeBundler/_nodeBundler-interface';
 
 export default function group(bundle: NodeBundle[]): GroupNode[] {
   const condition = !figma.command.includes('-one');
-  // const family = condition ? bundle : bundle[0].children;
 
   return (bundle as NodeBundle[]).reduce((acc: GroupNode[], item: NodeBundle | SceneNode) => {
     const parent = (item as NodeBundle).parent,
       childrens = (item as NodeBundle).children;
 
     if (condition) {
-      const order = getNodeIndex((item as NodeBundle).children[0]);
+      const orders = (item as NodeBundle).children.reduce((acc, cur) => {
+          acc.push(getNodeIndex(cur));
+          return acc;
+        }, [] as number[]),
+        order = Math.min(...orders);
+
       const group = figma.group(childrens as SceneNode[], parent);
       group.expanded = false;
       parent.insertChild(order, group);
       acc.push(group);
-      
+
       return acc;
     }
 
